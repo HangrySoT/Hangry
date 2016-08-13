bbe6e6cf0bed2307c145579aeba4ea648f831684
XizheQiu <xqiu318@aucklanduni.ac.nz>
2016-08-13 22:39:39 +1200

    LocationAlgorithm
    Basic algorithm to work out best restaurant based on distance/cost

--- HangrySoT.Website/HangrySoT.Tests/ApiClient/Zomato/ZomatoApiClientTest.cs
+++ HangrySoT.Website/HangrySoT.Tests/ApiClient/Zomato/ZomatoApiClientTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using HangrySoT.ApiClient.Zomato;
+using HangrySoT.Website.Services;
 using System.Threading.Tasks;
 using PowerAssert;
 using System.Linq;
@@ -14,10 +15,14 @@ namespace HangrySoT.Tests.ApiClient.Zomato
         {
             ZomatoClient client = new ZomatoClient();
             var zomatoObject = await client.SearchByLatLon();
+            //var xizheZomatoObject = await client.SearchByLatLon(-36.934186m, 174.939321m);
+            //var service = new SuperSecretSauceService();
+            //var updatedZomatoObject = service.GetBestRestaurant(xizheZomatoObject, -36.934186, 174.939321);
 
             PAssert.IsTrue(() => zomatoObject.restaurants.Any());
             PAssert.IsTrue(() => zomatoObject.restaurants.First().restaurant.location.longitude != null);
-
+            //PAssert.IsTrue(() => updatedZomatoObject.restaurants.First().restaurant.average_cost_for_two > 38);
+            //PAssert.IsTrue(() => updatedZomatoObject.restaurants.First().restaurant.average_cost_for_two < 41);
         }
     }
 }
---
+++ HangrySoT.Website/HangrySoT.Website/Services/SuperSecretSauceService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Location;
+using System.Linq;
+using System.Web;
+using HangrySoT.ApiClient.Models;
+
+namespace HangrySoT.Website.Services
+{
+    public class EmotionDetector
+    {
+        public double GetDistancePriceFactor(decimal anger, decimal contempt, decimal disgust, decimal fear, decimal happiness, decimal neutral, decimal sadness, decimal surprise)
+        {
+            
+            return 0;
+        }
+    }
+    public class SuperSecretSauceService
+    {
+
+        public string GetBestRestaurant(ZomatoResponse ZomatoResponse, double lng, double lat, double costPerMetre = 0.00244)
+        {
+            var userCoord = new GeoCoordinate(lat, lng);
+            string bestRestaurantID = "";
+            double bestRestaurantModifiedPriceRange = 0;
+            double bestRestaurantDistance = 0;
+
+            foreach (var restaurant in ZomatoResponse.restaurants)
+            {
+
+                var restaurantCoord = new GeoCoordinate(double.Parse(restaurant.restaurant.location.latitude), double.Parse(restaurant.restaurant.location.longitude));
+                double distance = userCoord.GetDistanceTo(restaurantCoord);
+                double pricePerPerson = restaurant.restaurant.average_cost_for_two / 2;
+                double processedPriceRange = (pricePerPerson + distance * costPerMetre) * 2;
+                if(bestRestaurantID == "")
+                {
+                    bestRestaurantID = restaurant.restaurant.id;
+                    bestRestaurantModifiedPriceRange = processedPriceRange;
+                    bestRestaurantDistance = distance;
+                }
+                else if (bestRestaurantModifiedPriceRange > processedPriceRange && distance < bestRestaurantDistance)
+                {
+                    bestRestaurantID = restaurant.restaurant.id;
+                    bestRestaurantModifiedPriceRange = processedPriceRange;
+                    bestRestaurantDistance = distance;
+                }
+            }
+            return bestRestaurantID;
+
+        }
+    }
+
+}
