2dfe9621a420a61ade9137db6765cd9164000b11
AaronYeoh <aaronyyh@gmail.com>
2016-08-13 16:27:10 +1200

    Adds Oxford Client

---
+++ HangrySoT.Website/HangrySoT.ApiClient/Oxford/OxfordClient.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.ProjectOxford.Emotion;
+using Microsoft.ProjectOxford.Emotion.Contract;
+
+namespace HangrySoT.ApiClient.Oxford
+{
+    public interface IOxfordClient
+    {
+        Task<Emotion[]> AnalyseImage(Stream imageFileStream);
+    }
+
+    public class OxfordClient : IOxfordClient
+    {
+        private const string subscriptionKey = "";
+        private readonly EmotionServiceClient _emotionServiceClient;
+
+        public OxfordClient()
+        {
+            _emotionServiceClient = new EmotionServiceClient(subscriptionKey);
+        }
+
+        public async Task<Emotion[]> AnalyseImage(Stream imageFileStream)
+        {
+            // Detect the emotions in the Stream
+            var emotionResult = await _emotionServiceClient.RecognizeAsync(imageFileStream);
+            return emotionResult;
+        }
+    }
+}
