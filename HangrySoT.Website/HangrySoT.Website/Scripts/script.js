c49dfeb97c33892d455f9ccecb4842816751a870
Nivlagx <galvin9@hotmail.com>
2016-08-14 10:32:24 +1200

    Created Location function

--- HangrySoT.Website/HangrySoT.Website/Scripts/script.js
+++ HangrySoT.Website/HangrySoT.Website/Scripts/script.js
@@ -1,1 +1,13 @@
-﻿
+﻿//Collect loaction in lat and lon
+navigator.geolocation.getCurrentPosition(foundLocation, noLocation);
+
+  function foundLocation(position)
+  {
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    alert('Found location: ' + lat + ', ' + lon);
+  }
+  function noLocation()
+ {
+    alert('Could not find location');
+ }
