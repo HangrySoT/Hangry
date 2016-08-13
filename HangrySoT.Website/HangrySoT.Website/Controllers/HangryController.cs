66ce7038ff89196b291b9af63efb7ca0ca74fe17
Gianni Camilleri <gianni.a.camilleri@gmail.com>
2016-08-13 20:25:33 +1200

    Emotion API Controller proof of concept (Gianni & Aaron) Uploads successfully posting to server.

--- HangrySoT.Website/HangrySoT.Website/Controllers/HangryController.cs
+++ HangrySoT.Website/HangrySoT.Website/Controllers/HangryController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using HangrySoT.ApiClient.Oxford;
+using Microsoft.ProjectOxford.Emotion.Contract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,10 +12,9 @@ namespace HangrySoT.Website.Controllers
 
     public class HangryInputModel
     {
-
         public double lat { get; set; }
         public double lon { get; set; }
-        public HttpPostedFile image { get; set; }       
+        public HttpPostedFileBase image { get; set; }       
 
     }
 
@@ -25,9 +27,36 @@ namespace HangrySoT.Website.Controllers
         }
 
         [HttpPost]
-        public ActionResult Index(HangryInputModel model)
+        public async System.Threading.Tasks.Task<ActionResult> Index(HangryInputModel model)
         {
-            return Content(model.image.ContentLength.ToString());
+            //string nameAndLocation = "~/UploadedFiles/" + model.image.FileName;
+            //model.image.SaveAs(Server.MapPath(nameAndLocation));
+
+            var filestream = model.image.InputStream;
+            var oxfordClient = new OxfordClient();
+            var emotions = await oxfordClient.AnalyseImage(filestream);
+
+            StringBuilder strb = new StringBuilder();
+
+            Scores scores;
+            int faceNo = 0;
+
+            foreach (var face in emotions)
+            {
+                faceNo++;
+                scores = face.Scores;
+                strb.Append("Face " + faceNo + "\n");
+                strb.Append("Anger: " + face.Scores.Anger + "\n");
+                strb.Append("Happiness: " + scores.Happiness + "\n");
+                strb.Append("Contempt: " + scores.Contempt + "\n");
+                strb.Append("Sadness: " + scores.Sadness + "\n");
+                strb.Append("Surprise: " + scores.Surprise + "\n");
+                strb.Append("Fear: " + scores.Fear + "\n");
+                strb.Append("\n\n");
+            }
+
+            //Response.TransmitFile(Server.MapPath(nameAndLocation));
+            return Content(strb.ToString(),"text/plain");
         }
     }
 }
