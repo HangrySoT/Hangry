9efe393750d41307f01bb9f38ca04c1e2c7bbd79
Gianni Camilleri <gianni.a.camilleri@gmail.com>
2016-08-13 17:00:40 +1200

    Created HangryController <In Progress>

---
+++ HangrySoT.Website/HangrySoT.Website/Controllers/HangryController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HangrySoT.Website.Controllers
+{
+
+    public class HangryInputModel
+    {
+
+        public double lat { get; set; }
+        public double lon { get; set; }
+        public HttpPostedFile image { get; set; }       
+
+    }
+
+    public class HangryController : Controller
+    {
+        // GET: Hangry
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(HangryInputModel model)
+        {
+            return Content(model.image.ContentLength.ToString());
+        }
+    }
+}
